22cae50797067e3e90d0a2b43b071c607e3acc9b
Adrian Wilke <adibaba@users.noreply.github.com>
2019-05-02 16:56:02 +0200

    ISO 639-1 language codes. v 1.0.1.2

---
+++ src/main/java/org/dice_research/opal/common/constants/ISO_639_1.java
@@ -0,0 +1,17 @@
+package org.dice_research.opal.common.constants;
+
+/**
+ * Language codes used in DCAT.
+ * 
+ * @see https://www.w3.org/TR/vocab-dcat/#Property:catalog_language
+ * 
+ * @author Adrian Wilke
+ */
+public abstract class ISO_639_1 {
+
+	public static final String DE = "http://id.loc.gov/vocabulary/iso639-1/de";
+	public static final String EN = "http://id.loc.gov/vocabulary/iso639-1/en";
+	public static final String FR = "http://id.loc.gov/vocabulary/iso639-1/fr";
+	public static final String ES = "http://id.loc.gov/vocabulary/iso639-1/es";
+
+}
